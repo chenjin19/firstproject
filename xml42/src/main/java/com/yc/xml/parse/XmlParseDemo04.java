bae53630a84233a8ae11a9743bbb3a1addec4693
jp86 <413670706@qq.com>
2017-07-06 15:47:30 +0800

    Signed-off-by: jp86 <413670706@qq.com>

--- xml42/src/main/java/com/yc/xml/parse/XmlParseDemo04.java
+++ xml42/src/main/java/com/yc/xml/parse/XmlParseDemo04.java
@@ -3,5 +3,7 @@ package com.yc.xml.parse;
 public class XmlParseDemo04 {
 	public static void main(String[] args) {
 		System.out.println("hello World");
+		System.out.println("hello World");
+		System.out.println("hello jp");
 	}
 }
