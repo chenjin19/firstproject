1b3977cc4e6862fb33e98b3ca27bedec47da2a03
jp86 <413670706@qq.com>
2017-07-06 15:53:07 +0800

    hello yc

--- xml42/src/main/java/com/yc/xml/parse/XmlParseDemo04.java
+++ xml42/src/main/java/com/yc/xml/parse/XmlParseDemo04.java
@@ -4,5 +4,6 @@ public class XmlParseDemo04 {
 	public static void main(String[] args) {
 		System.out.println("hello World");
 		System.out.println("hello World");
+		System.out.println("hello yc");
 	}
 }
